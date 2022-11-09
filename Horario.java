3b67543931fe453538f303cc69688e566c8c3b65
LeoConQui <con18797@uvg.edu.gt>
2022-11-09 10:27:55 -0600

    Update Horario.java
    Modificaciones a la clase horario

--- Horario.java
+++ Horario.java
@@ -1,28 +1,39 @@
 import java.util.ArrayList;
 
-public class Horario extends Intervalo {
-    private ArrayList<Intervalo> intervalos;
-
-    public Horario() {
+ /**
+  * Horario es una clase que simula la agenda de una persona.
+  * @author Leonel (18797)
+  * @author Ian (22128)
+  * @version 1.0
+  */
  
 
-        //Método
-        intervalos = new ArrayList<Intervalo>();
-            intervalos.add(horarioInicio.Intervalo);
-            intervalos.add(horarioFin.Intervalo);
-            intervalos.add(persona.Intervalo);
-            intervalos.add(estado.Intervalo);
-        
-            //Sets y Gets
-            public Intervalo getintervalos(){
-                return intervalos;
-            }
+public class Horario {
+
+    // atributos 
+    private ArrayList<Intervalo> intervalos;
+
+    // meteodos 
+
+    /** Horario es el contrustor de la clase; no tiene parametros
+     * @return al ser constructor, no tiene retorno
+     */
+
+    public Horario() {
     
-            public void setintervalos(Intervalo intervalos) {
-                this.intervalos = intervalos;
-            }
-        
-            
+        intervalos = new ArrayList<Intervalo>();
     }
+
+    /** Agregarintervalo es un metodo que agrega un intervalo al array de intervalos 
+     * @param fecha es la fecha en que se va a realizar la cita
+     * @param horainicio es la hora de inicio de la cita
+     * @param horafinal es la hora en que termina la reunion
+     * @param Persona es la persona con quien se va a tener la cita
+     * @return void
+     */
+
+     public void Agregarintervalo() {
+         
+     }
 
 }
